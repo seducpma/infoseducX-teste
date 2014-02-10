6fa6dfe5720ff5e65c7e5645c7d8d09b442fd064
seduc <seduc@seduc1.(none)>
2014-02-10 14:25:46 -0200

    oficios 10/02/14

--- app/models/oficio.rb
+++ app/models/oficio.rb
@@ -1,10 +1,9 @@
 class Oficio < ActiveRecord::Base
 
-
 after_create :geracodigo
 
 def geracodigo
-    self.codigo = [self.id+6].to_s + ("/2014")
+    self.codigo = [self.id-11].to_s + ("/2014")
     self.save
 
 end
