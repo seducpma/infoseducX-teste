970fa537584ce4a83a763950ecc23cee2ff48d62
seduc <seduc@seduc1.(none)>
2014-03-12 10:10:54 -0300

    cursos 12/03/14

--- app/controllers/relatestagiarios_controller.rb
+++ app/controllers/relatestagiarios_controller.rb
@@ -6,14 +6,33 @@ class RelatestagiariosController < ApplicationController
   layout "application"
     
 def load_unidades
-  @unidades = Unidade.find(:all, :order => 'nome ASC')
+  if current_user.unidade_id == 100 or current_user == 53
+    @unidades = Unidade.find(:all,:order => 'nome ASC')
+  else
+    @unidades = Unidade.find(:all, :conditions =>  ["id= ?",current_user.unidade_id],:order => 'nome ASC')
+  end
 end
 
 
 def load_estagiarios
-    @estagiarios = Estagiario.find(:all, :conditions =>  ["desligado=0 and flag =0"], :order => 'nome ASC')
+    @estagiarios = type_user(current_user.unidade_id)
   end
 
+  def type_user(unit)
+    if  unit == 53 or unit == 100
+      # type = 53 => usuário administrativo
+      @estagiarios = Estagiario.find(:all, :conditions =>  ["desligado=0 and flag = 0"], :order => 'nome ASC')
+    else
+      if unit == 99
+        # type = 99 => usuário itinerante
+        #@regiao = Regiao.all
+        @estagiarios = Estagiario.find(:all, :conditions =>  ["desligado=0 and flag = 0 and unidade_id is ?", nil], :order => 'nome ASC')
+      else
+        @estagiarios = Estagiario.find(:all, :conditions =>  ["desligado=0 and flag = 0 and unidade_id = ?", unit], :order => 'nome ASC')
+      end
+    end
+  end
+  
   def index
     @relatestagiarios = Relatestagiario.find(:all, :joins => :unidade, :conditions => ["unidades.nome like ?", "%" + params[:search].to_s + "%"], :order => 'datahora DESC')
     respond_to do |format|
