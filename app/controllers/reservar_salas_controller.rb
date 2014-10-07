188460107cab65f6a8aad4e295c3259c049285ba
seduc <seduc@seduc1.(none)>
2014-10-07 14:45:13 -0300

    acertos agendamentos

--- app/controllers/reservar_salas_controller.rb
+++ app/controllers/reservar_salas_controller.rb
@@ -5,10 +5,10 @@ class ReservarSalasController < ApplicationController
 before_filter :load_salas
 before_filter :load_servicos_salas
 layout :define_layout
-before_filter :login_required, :except => ["index", "show", "create", "new","edit","detalhes"]
+before_filter :login_required, :except => ["index", "show", "create", "new","edit","sel_dados"]
 
  def load_servicos_salas
-  @servicos_salas = ServicosSala.find(:all)
+  @servicos_salas = ServicosSala.find(:all, :conditions=>['status = 1'] )
   end
 
  def load_salas
@@ -54,21 +54,52 @@ before_filter :login_required, :except => ["index", "show", "create", "new","edi
 
   # POST /reservar_salas
   # POST /reservar_salas.xml
-  def create
-    @reservar_sala = ReservarSala.new(params[:reservar_sala])
 
-    respond_to do |format|
-      if @reservar_sala.save
-        flash[:notice] = 'Cadastrado com sucesso.'
-        format.html { redirect_to(@reservar_sala) }
-        format.xml  { render :xml => @reservar_sala, :status => :created, :location => @reservar_sala }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @reservar_sala.errors, :status => :unprocessable_entity }
-      end
+def create
+
+    @reservar_sala = ReservarSala.new(params[:reservar_sala])
+    $salareserva = @reservar_sala.sala_id
+    $datareserva = @reservar_sala.data_reserva
+    $horareservai = (@reservar_sala.horario_reservai).to_time
+    $horareservaf = (@reservar_sala.horario_reservaf).to_time
+    @reservado =ReservarSala.find(:all, :conditions => ['sala_id = ? and data_reserva = ? and horario_reservai <= ? and horario_reservaf >= ?', $salareserva, $datareserva, $horareservai, $horareservai  ])
+    if (@reservar_sala.horario_reservaf.hour < @reservar_sala.horario_reservai.hour)
+      respond_to do |format|
+       flash[:notice] = 'HORÁRIO DE ENCERRAMENTO MAIOR QUE HORÁRIO DE INICIO.'
+       format.html { redirect_to new_reservar_sala_path }
+       format.xml  { head :ok }
+     end
+   else
+    if   (@reservar_sala.data_reserva >= (DateTime.now.to_date + 1))
+     if ((@reservado.present? ))
+     respond_to do |format|
+       flash[:notice] = 'JÁ EXISTE RESERVA PARA ESTE DIA E HORA.'
+       format.html { redirect_to reservar_salas_path }
+       format.xml  { head :ok }
+     end
+    else
+     respond_to do |format|
+          if @reservar_sala.save
+            flash[:notice] = 'Cadastrado com sucesso.'
+            format.html { redirect_to(@reservar_sala) }
+            format.xml  { render :xml => @reservar_sala, :status => :created, :location => @reservar_sala }
+          else
+            format.html { render :action => "new" }
+            format.xml  { render :xml => @reservar_sala.errors, :status => :unprocessable_entity }
+          end
+        end
+    end
+    else
+       respond_to do |format|
+       flash[:notice] = 'RESERVA DEVE SER FEITA COM 24 HORAS DE ANTECEDÊNCIA.'
+       format.html { redirect_to reservar_salas_path }
+       format.xml  { head :ok }
+     end
     end
   end
+ end
 
+  
   # PUT /reservar_salas/1
   # PUT /reservar_salas/1.xml
   def update
@@ -99,7 +130,7 @@ before_filter :login_required, :except => ["index", "show", "create", "new","edi
   end
 
 
-   def define_layout
+  def define_layout
     if logged_in?
       'application'
     else
@@ -108,12 +139,18 @@ before_filter :login_required, :except => ["index", "show", "create", "new","edi
   end
 
 
-def detalhes
-     $sala = params[:reservar_sala_sala_id]
-     @dados = Sala.find(:all, :conditions => ['id=' + $sala])
-   render update do |page|
-      page.replace_html 'especifica', :partial => 'especificacao'
-    end
+
+ def sel_dados
+    @dados = Sala.find(params[:reservar_sala_sala_id])
+    session[:reservasala]= params[:reservar_sala_sala_id]
+    render :partial => 'exibe_dados'
+    #render :update do |page|
+    #  page.replace_html "especifica", :partial => 'exibe_dados'
+    #end
   end
 
-  end
+ def sel_data
+     session[:reservadata]= params[:reservar_sala_data_reserva]
+ end
+
+end
