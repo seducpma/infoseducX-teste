fab399c1b46cc2eefc7e7dd2f32265c2b73a0f22
seduc <seduc@seduc1.(none)>
2015-01-19 08:48:21 -0200

    acerto

--- app/models/servicos_interno.rb
+++ app/models/servicos_interno.rb
@@ -3,7 +3,7 @@ class ServicosInterno < ActiveRecord::Base
   after_create :geracodigo
 
 def geracodigo
-    self.codigo = [self.id-21].to_s + ("/2015")
+    self.codigo = [self.id-244].to_s + ("/2015")
     self.save
 
 end
