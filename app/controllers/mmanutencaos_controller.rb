0912d74102ea1887d21c13ae24a535224076c6fc
Seduc <seduc@pspma.(none)>
2016-10-25 13:47:12 -0200

    25-10-16

--- app/controllers/mmanutencaos_controller.rb
+++ app/controllers/mmanutencaos_controller.rb
@@ -45,7 +45,6 @@ class MmanutencaosController < ApplicationController
   def index
     if current_user.has_role?('administrador') or current_user.has_role?('admin_manutencao')
        @mmanutencaos = Mmanutencao.all(:conditions =>  "situacao_manutencao_id <> 2")
-       @mmanutencaos_unidade = Mmanutencao.all(:conditions => ["situacao_manutencao_id <> 2"])
     else
       if current_user.has_role?('diretor_unidade')
 #        $cont1=0
@@ -58,11 +57,10 @@ class MmanutencaosController < ApplicationController
 #       @contador = Mmanutencao.all.count
 #       @mmanutencaos = Mmanutencao.all(:conditions =>["situacao_manutencao_id <> 2 and unidade_id = ?",current_user.unidade_id ])
        @mmanutencaos = Mmanutencao.all(:conditions =>  "situacao_manutencao_id <> 2")
-       @mmanutencaos_unidade = Mmanutencao.all(:conditions => ["situacao_manutencao_id <> 2 and unidade_id=?", current_user.unidade_id])
       else
 
         @mmanutencaos = Mmanutencao.all(:conditions =>["situacao_manutencao_id <> 2 and user_id = ?",current_user])
-        @mmanutencaos_unidade = Mmanutencao.all(:conditions => ["situacao_manutencao_id <> 2 and unidade_id=?", current_user.unidade_id])
+
        #$chefia1=@mmanutencaos.user_id.current_user
       end
     end
