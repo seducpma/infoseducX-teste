fb53e6ba7f1794e6e2fa82f66ab1342ec08c7278
Your Name <you@example.com>
2018-09-13 11:39:12 -0300

    2018-09-12-1227

--- app/controllers/orc_pedido_compras_controller.rb
+++ app/controllers/orc_pedido_compras_controller.rb
@@ -11,7 +11,7 @@ class OrcPedidoComprasController < ApplicationController
         if current_user.has_role?('admin') or current_user.has_role?('SEDUC')
            @orc_pedido_si= OrcPedidoCompra.find(:all, :select => 'codigo', :conditions => ['ano= ? ', Time.now.year], :order => 'id DESC')
         else if (current_user.login = 'merenda.adriana')or (current_user.login = 'merenda.fabiana') #excessão dos usuáriosda merenda
-              @orc_pedido_si= OrcPedidoCompra.find(:all, :select => 'codigo', :conditions => ['ano= ? and (user_id = ?  AND user_id = ?) ', Time.now.year, 523, 525 ], :order => 'id DESC')
+              @orc_pedido_si= OrcPedidoCompra.find(:all, :select => 'codigo', :conditions => ['ano= ? and (user_id = 523  or user_id = 525) ', Time.now.year], :order => 'id DESC')
 
            else
                 @orc_pedido_si= OrcPedidoCompra.find(:all, :select => 'codigo', :conditions => ['ano= ? and user_id = ?', Time.now.year, current_user.id], :order => 'id DESC')
