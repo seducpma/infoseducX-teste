66e108a990fc140504ed48d01efcab37963a55f4
Your Name <you@example.com>
2019-01-02 13:19:13 -0200

    2019-01-02

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 #set :application, "192.168.0.12"
 
 
-#set :application, "192.168.0.14"  # 192.168.0.14 - Servidor oficial (hera)  # 177.36.159.102 - Servidor Teste Alvaro/Alexandre
+set :application, "192.168.0.14"  # 192.168.0.14 - Servidor oficial (hera)  # 177.36.159.102 - Servidor Teste Alvaro/Alexandre
 
-set :application, "177.36.136.46"
+#set :application, "177.36.136.46"
 
 
 #set :repository, "git://github.com/seducpma/sisgered.git"
