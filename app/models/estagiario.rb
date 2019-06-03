6f2c81a0fb4766e234ad0210dc15582551e157a6
Your Name <you@example.com>
2019-06-03 14:50:18 -0300

    31-05-19

--- app/models/estagiario.rb
+++ app/models/estagiario.rb
@@ -7,9 +7,8 @@ class Estagiario < ActiveRecord::Base
   before_save  :maiusculo, :com_est, :if => :verify?
   before_update :sai, :if => :verify?
   has_attached_file :photo, :styles => {:thumb=> "100x100#", :small  => "150x150>" },
-                    :url => "/photos/estagiarios/:id.:extension",
-                    :path => ":rails_root/public/photos/estagiarios/:id.:extension"
-
+                    :url => "/:id.:extension",
+                    :path => ":rails_root/public/:id.:extension"
 
 before_save :teste
 
