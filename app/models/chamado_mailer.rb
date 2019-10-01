560e85b9c2e435ed300c4a7c085e42fb55d08425
seducpma <naor@seducpma.com>
2019-10-01 12:24:44 -0300

    2019-10-01-1224

--- app/models/chamado_mailer.rb
+++ app/models/chamado_mailer.rb
@@ -3,7 +3,7 @@ class ChamadoMailer < ActionMailer::Base
 		setup_email(user)
 		@subject    += '. Você solicitou a mudança da sua senha'
     if RAILS_ENV == "production"
-      @body[:url]  = "http://sisgered.seducpma.com/reset_password/#{user.password_reset_code}"
+      @body[:url]  = "http://infoseduc.seducpma.com/reset_password/#{user.password_reset_code}"
     else
       @body[:url]  = "http://localhost:3000/reset_password/#{user.password_reset_code}"
     end
@@ -11,6 +11,7 @@ class ChamadoMailer < ActionMailer::Base
 	end
 
   protected
+
     def setup_email(user)
       @recipients  = "#{user.email}"
       @from        = "no-reply@informatica@seducpam.com"
