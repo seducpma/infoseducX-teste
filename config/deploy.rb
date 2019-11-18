7eb32859a79918c14928c30d2a711340a35d0b40
seducpma <naor@seducpma.com>
2019-11-18 16:34:29 -0200

    2019-11-18-1634

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 #set :application, "192.168.0.12"
 
 
-set :application, "192.168.0.14"  # 192.168.0.14 - Servidor oficial (hera)  # 177.36.159.102 - Servidor Teste Alvaro/Alexandre
+set :application, "192.168.10.36"  # 192.168.0.14 - Servidor oficial (hera)  # 177.36.159.102 - Servidor Teste Alvaro/Alexandre
 
-set :application, "200.168.214.58"
+set :application, "170.81.237.114"
 
 
 #set :repository, "git://github.com/seducpma/sisgered.git"
-set :repository, "git://github.com/seducpma/infoseducX.git"
-set :user, "atena"
+set :repository, "git://github.com/seducpma/infoseducX-teste.git"
+set :user, "inf"
 set :use_sudo, false
 #set :deploy_to, "/home/#{user}/sisgered.seducpma.com"
-set :deploy_to, "/home/#{user}/infoseduc.seducpma.com"
+set :deploy_to, "/home/#{user}/infoseducX-teste.seducpma.com"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
@@ -23,7 +23,7 @@ server application, :app, :web, :db, :primary => true
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
      #run "ln -s /home/atena/photos/infoseduc /home/atena/infoseduc.seducpma.com/current/public/photos"
-     run "ln -s /home/atena/photos/infoseduc /home/atena/infoseduc.seducpma.com/current/public/photos"    # direcionamento da pasta para guardar imagens
+     run "ln -s /home/atena/photos/infoseduc /home/atena/infoseducX-teste.seducpma.com/current/public/photos"    # direcionamento da pasta para guardar imagens
      #run "ln -s /home/atena/photos/sisgered /home/atena/sisgered.seducpma.com/current/public/photos"
 
 
